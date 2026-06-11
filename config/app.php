069c53cf19596f68a8610e9bd108bf7932a0091e
le-yo <le-yo@le-yo.com>
2026-06-11 23:12:17 +0300

    Fix admin_key — add to config/app.php so it survives config:cache

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'admin_key' => env('ADMIN_KEY'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
